eb19e0a357285533489b22e4aa7ec1bb09a02412
cintiayaritza <70987201+cintiayaritza@users.noreply.github.com>
2020-12-03 15:08:05 -0500

    terminal de yash-parcial3

---
+++ yash.sh
@@ -0,0 +1,21 @@
+
+#!/bin/bash
+
+#prediccion de la linea de comandos basada en le historial de comandos
+
+#array-matrices
+#numeros fraccionarios y expasion aritmetricas
+
+#redireccion de socket,redirecciones de canalizacion y directorio de procesos
+
+#comandos de solicitud y controlador 
+#comandos no encontrador
+
+#un shell de linea de comandos compatibles con posix con soporte
+#integrador para a completar y predecir segun el historia de comandos
+
+
+
+to_print="hola mundo"
+echo $to_print
+#hola mundo
